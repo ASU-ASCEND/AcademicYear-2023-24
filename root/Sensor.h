e7d6cfb56784efc7726abdaddb41d0a65e103f27
Muhammed Hunaid Topiwala <muhammedhunaid@gmail.com>
2024-02-09 17:11:14 -0700

    Update Sensor.h

--- root/Sensor.h
+++ root/Sensor.h
@@ -2,12 +2,15 @@
 #define SENSOR_H
 
 #include <Arduino.h> //Needed for Arduino functionalities
+#include <string> //Needed for returning string from writeData()
 
 class Sensor {
 public:
-//Accessor Method to access name of sensor
-virtual const String& getSensorName() const = 0;
-//Accessor Method to access status of successful pin connection 
-virtual bool verifyPin();
+  //Accessor Method to access name of sensor
+  virtual const String& getSensorName() const = 0;
+  //Accessor Method to access status of successful pin connection 
+  virtual bool verifyPin();
+  //Accessor Method to access observed environmental changes
+  virtual std::string readData() = 0;
 };
 #endif
