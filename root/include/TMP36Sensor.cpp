48388cca5f35e3dd19a92b78dbe494c941fa2fce
hsing117 <145151413+hsing117@users.noreply.github.com>
2024-02-23 16:15:23 -0700

    Update TMP36Sensor.cpp

--- root/include/TMP36Sensor.cpp
+++ root/include/TMP36Sensor.cpp
@@ -4,6 +4,13 @@
 const std::string& TMP36Sensor::getSensorName() const {
   return nameCompiled;
 }
+
+//implements the constructor
+TMP36Sensor::TMP36Sensor() {
+    nameCompiled = "TMP36"; 
+}
+
+  TMP36
 //Implementing virtual function: verifyPin()
 bool TMP36Sensor::verifyPin() override{
   return true;
