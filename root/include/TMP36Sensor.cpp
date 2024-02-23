4339b2004c07d3d3ed290554826c649baed5280e
hsing117 <145151413+hsing117@users.noreply.github.com>
2024-02-23 16:28:42 -0700

    Update TMP36Sensor.cpp

--- root/include/TMP36Sensor.cpp
+++ root/include/TMP36Sensor.cpp
@@ -1,13 +1,13 @@
 #include <TMP36Sensor.h>
 
-//Implementing virtual function: getSensorName()
-const std::string& TMP36Sensor::getSensorName() const {
-  return nameCompiled;
-}
-
 //implements the constructor
 TMP36Sensor::TMP36Sensor() {
     nameCompiled = "TMP36"; 
+}
+
+//Implementing virtual function: getSensorName()
+const std::string& TMP36Sensor::getSensorName() const {
+  return nameCompiled;
 }
 
   TMP36
