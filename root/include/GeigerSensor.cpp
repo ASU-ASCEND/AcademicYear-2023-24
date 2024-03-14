9cf7a59ff844abd353d753cb690fa5c1584c2d01
SnailDragon <tylernielsenn@gmail.com>
2024-03-14 10:14:07 -0700

    expanded Geiger code using serial input

--- root/include/GeigerSensor.cpp
+++ root/include/GeigerSensor.cpp
@@ -16,11 +16,27 @@ String GeigerSensor::readData(){
     Serial.begin(9600, SERIAL_8N1); // default protocal - added to be explicit 
 
     // reads serial output --> "CPS, #####, CPM, #####, uSv/hr, ###.##, SLOW|FAST|INST"
-    String input = Serial.readStringUntil('\r\n'); // I think it's deliminated by CRLF (carriage return, line feed)
-    Serial.println(input);
+    // I think it's deliminated by CRLF (carriage return, line feed)
+    Serial.readStringUntil('\r\n'); // flush input (we don't know where in the line we're connecting
+    String input = Serial.readStringUntil('\r\n'); // read the line 
+    Serial.println(input);  // print it 
     // for(int i = 0; i < 7; i++){ 
     //     String input = Serial.readStringUntil(','); 
     //     Serial.println(input); 
     // }
+    
+    String str = "";
+    String values[7];
+    int curr = 0;
+    for(int i = 0; i < input.length(); i++){
+        str += input[i];
+        if(input[i] == ','){
+            values[curr] = str;
+            str = "";
+            curr++;
+        }
+    }
+
+    return values[1] + values[3] + values[5] + values[6];
     
 }
