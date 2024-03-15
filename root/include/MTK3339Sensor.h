9f1510ed83c66cef757ba50e9d69c56c01115c81
SnailDragon <tylernielsenn@gmail.com>
2024-03-15 11:57:29 -0700

    typo fixing COMPILES

--- root/include/MTK3339Sensor.h
+++ root/include/MTK3339Sensor.h
@@ -10,9 +10,11 @@ class MTK3339Sensor : public Sensor {
 private:
     //Name for the sensor to be used in CSV header and console
     static constexpr const char* nameCompiled = "MTK3339";
+    static constexpr const char* csvHeaderCompiled = "MTK3339";
     
 public:
     const String& getSensorName() const override;
+    const String& getSensorCSVHeader() const;
     bool verifyPin() override;
     String readData() override; 
 };
