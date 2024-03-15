1912ad25a6d241432887f20a686c9c0aaf63fb19
SnailDragon <tylernielsenn@gmail.com>
2024-03-15 11:50:02 -0700

    cleaned up after merge

--- root/include/SDCard.h
+++ root/include/SDCard.h
@@ -9,12 +9,8 @@ private:
   //Name for the sensor to be used in CSV header and console
   static constexpr const char* sdCardName = "SDCard";
 public:
-<<<<<<< HEAD
   bool verifyPin();
   const String& getSDCardName() const;
-=======
-  const arduino::String& getSDCardName() const;
->>>>>>> main
 };
 
 #endif
--- root/src/MTK3339Sensor.cpp
+++ root/src/MTK3339Sensor.cpp
@@ -2,29 +2,18 @@
 
 //Implementing virtual function: getSensorName()
 const String& MTK3339Sensor::getSensorName() const {
-<<<<<<< HEAD:root/src/MTK3339Sensor.cpp
   return nameCompiled;
-=======
-    return nameCompiled;
->>>>>>> main:root/include/MTK3339Sensor.cpp
 }
 //Implementing virtual function: getSensorType()
 const String& MTK3339Sensor::getSensorCSVHeader() const {
     return csvHeaderCompiled;
 }
 //Implementing virtual function: verifyPin()
-<<<<<<< HEAD:root/src/MTK3339Sensor.cpp
 bool MTK3339Sensor::verifyPin() {
   return true; 
 }
+
 //Implementing virtual function: readData()
 String MTK3339Sensor::readData(){
   return ", ";
-=======
-bool MTK3339Sensor::verifyPin() override {
-    return true;
-}
-//Implementing virtual function: readData()
-String MTK3339Sensor::readData(){
->>>>>>> main:root/include/MTK3339Sensor.cpp
 }
