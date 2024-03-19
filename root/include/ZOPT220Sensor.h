1cb85ae70f6e08003173f89e81d47627bfa6a679
SnailDragon <tylernielsenn@gmail.com>
2024-03-18 20:42:12 -0700

    added code for UV index sensor

---
+++ root/include/ZOPT220Sensor.h
@@ -0,0 +1,21 @@
+#ifndef ZOPT220SENSOR_H
+#define ZOPT220SENSOR_H
+
+#include "Sensor.h"
+#include "ZOPT220x_Control.ino"
+#include <string.h>
+#include <Arduino.h>
+#include <Wire.h>
+
+class ZOPT220Sensor : public Sensor {
+    private:
+        static constexpr const char* nameCompiled = "ZOPT220";
+        static constexpr const char* csvHeaderCompiled = "ZOPT220 , ";
+    public:
+        const arduino::String& getSensorName() const override;
+        const arduino::String& getSensorCSVHeader() const; 
+        bool verifyPin() override;
+        arduino::String readData() override;
+};
+
+#endif
---
+++ root/src/ZOPT220Sensor.cpp
@@ -0,0 +1,41 @@
+#include "ZOPT220Sensor.h"
+
+const arduino::String& ZOPT220Sensor::getSensorName() const {
+    return nameCompiled;
+}
+
+const arduino::String& ZOPT220Sensor::getSensorCSVHeader() const {
+    return csvHeaderCompiled;
+} 
+
+bool ZOPT220Sensor::verifyPin(){
+    return zopt220xSetup();
+}
+
+arduino::String ZOPT220Sensor::readData() {
+    // UV index
+    setResolution(2);
+    setGain(4);
+    enableUVBSensing();
+
+    // might need delay
+
+    float uvIndex = getUVIndex(); // can be converted to UV Index
+
+    // ALS - Abient Light 
+    setResolution(2);
+    setGain(1);
+    enableALSSensing();
+
+    // might need delay
+    int it = 0;
+    while(!dataAvailable() && it < 40){
+        delay(10);
+        it++;
+    }
+
+    long als = getALS();
+
+    return String(uvIndex) + ", " + String(als) + ", "; 
+
+}
